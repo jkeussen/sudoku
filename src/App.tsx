33a34d56b5c59d08fa9635e035a9601f04b06753
Boris Boguslavsky <boris@boguslavsky.design>
2022-12-06 23:37:25 -0600

    Implemented solving/generation

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@ import { useEffect, useMemo, useState } from 'react'
 
 import Header from './components/Header';
 import Tile from './components/Tile';
+import MobileInputs from './components/MobileInputs';
 
 import { sectionRefArr } from './helpers/get-section';
 import { getValidRows, getValidCols, getValidSections } from './helpers/get-valid-areas';
 import { getGlobalErrors, getLocalErrors } from './helpers/get-errors';
 import { empty } from './helpers/valid-inputs';
 
+import { getSudoku } from './lib/sudoku';
+import { generateSudoku } from './lib/generate-unique-puzzle';
+
 import classes from './App.module.css'
-import MobileInputs from './components/MobileInputs';
+
+const sudoku = getSudoku();
 
 function App() {
   
-  const rawPuzzle = ".1...8...3.472169...6....1....9.253..421.378..358.6....9....1...213874.9...5...2."
+  const [rawPuzzle, setRawPuzzle] = useState(generateSudoku('insane')[0])
+  // const [rawPuzzle, setRawPuzzle] = useState(".1...8...3.472169...6....1....9.253..421.378..358.6....9....1...213874.9...5...2.")
+  console.log(generateSudoku('easy'))
   
   const buildGrid = (str: string): string[][] => {
     let arr = [];
@@ -36,13 +43,6 @@ function App() {
   const updateUserPuzzle = (val: string, row: number, col: number): void => {
     let newUserPuzzle = [...userPuzzle]
     newUserPuzzle[row][col] = val
-    // check for valid rows and columns
-    setValidRows(getValidRows(newUserPuzzle))
-    setValidCols(getValidCols(newUserPuzzle))
-    setValidSections(getValidSections(newUserPuzzle))
-    // setValidRows(validRows)
-    // setValidCols(validCols)
-    // update current puzzle state
     setUserPuzzle(newUserPuzzle)
   }
 
@@ -55,6 +55,10 @@ function App() {
 
   useEffect(() => {
     setGlobalErrors(new Set(getGlobalErrors(userPuzzle)))
+    // check for valid rows and columns
+    setValidRows(getValidRows(userPuzzle))
+    setValidCols(getValidCols(userPuzzle))
+    setValidSections(getValidSections(userPuzzle))
     // console.log('Global Errors: \n', globalErrors)
   }, [userPuzzle])
 
@@ -96,6 +100,14 @@ function App() {
     return dividers;
   }
   const dividers = useMemo(() => generateDividers(), [])
+
+  const solvePuzzle = (puzzle: string[][]) => {
+    let string = sudoku.board_grid_to_string(puzzle)
+    console.log(string)
+    let solvedString = sudoku.solve(string)
+    let solvedGrid = sudoku.board_string_to_grid(solvedString)
+    setUserPuzzle(solvedGrid)
+  }
   
   // return <Tile key={`gridTile_${index}`} value={char} />
   return (
@@ -106,6 +118,9 @@ function App() {
         {puzzle}
       </div>
       <MobileInputs />
+      <button onClick={() => solvePuzzle(userPuzzle)}>
+        Solve
+      </button>
     </div>
   )
 }
