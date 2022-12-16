b569f697338a2ed3544327be74f9387f449d88a2
Boris Boguslavsky <boris@boguslavsky.design>
2022-12-15 23:03:53 -0600

    Cleanup and organization

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -2,31 +2,12 @@ import classes from "./Controls.module.css";
 
 import InputTopRow from "./Controls/InputTopRow";
 import NumberInputs from "./Controls/NumberInputs";
-import GameActions from "./Controls/GameActions";
-import GameDifficultySelect from "./Controls/GameDifficultySelect";
-import OptionsSwitches from "./Controls/OptionsSwitches";
-
-import { useAppSelector } from "../store/hooks";
 
 const Controls = () => {
-
-	const showDifficultySelect = useAppSelector(
-		(state) => state.ui.showDifficultySelect
-	);
-
 	return (
 		<div className={classes.controls}>
-			{/* <div className={classes.col1}> */}
-				<NumberInputs className={classes.numbers} />
-				<InputTopRow className={classes.topRow} />
-			{/* </div> */}
-			{/* <div className={classes.col2}>
-				<GameActions className={classes.gameActions} />
-				{showDifficultySelect && (
-					<GameDifficultySelect className={classes.difficultySelect} />
-				)}
-				<OptionsSwitches className={classes.switches} />
-			</div> */}
+			<NumberInputs className={classes.numbers} />
+			<InputTopRow className={classes.topRow} />
 		</div>
 	);
 };
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,24 +1,24 @@
-import Modal from "./Modal";
-
-import buttonCss from '../styles/Buttons.module.css'
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { uiActions } from "../store/ui-slice";
 import { puzzleActions } from "../store/puzzle-slice";
+
+import Modal from "./Modal";
+import GameDifficultySelect from "./Controls/GameDifficultySelect";
 import OptionsSwitches from "./Controls/OptionsSwitches";
 
-import controlsCss from './Controls.module.css'
-import GameDifficultySelect from "./Controls/GameDifficultySelect";
+import buttonCss from "../styles/Buttons.module.css";
+import classes from './Menu.module.css'
 
 const Menu = () => {
-
 	const dispatch = useAppDispatch();
 
-	const buttonStyle = `${buttonCss.button}`
+	const buttonStyle = `${buttonCss.button}`;
 
 	const onCloseHandler = () => {
-		console.log('close menu modal')
-		dispatch(uiActions.setShowMenu(false))
-	}
+		console.log("close menu modal");
+		dispatch(uiActions.collapseMenuItems());
+		dispatch(uiActions.setShowMenu(false));
+	};
 
 	const showDifficultySelect = useAppSelector(
 		(state) => state.ui.showDifficultySelect
@@ -30,7 +30,6 @@ const Menu = () => {
 
 	const showSolutionHandler = () => {
 		onCloseHandler();
-		dispatch(uiActions.collapseMenuItems());
 		dispatch(puzzleActions.solvePuzzle());
 	};
 
@@ -42,27 +41,43 @@ const Menu = () => {
 	const showOptionsHandler = () => {
 		dispatch(uiActions.collapseMenuItems());
 		dispatch(uiActions.setShowOptionsSwitches(!showOptionsSwitches));
-	}
+	};
 
-	return(<Modal 
-		title="Menu"
-		noBackButton={true}
-		onBack={() => {}}
-		noCloseButton={false}
-		onClose={onCloseHandler}
-	>
-		<button className={`${buttonStyle} ${showDifficultySelect ? buttonCss.pressed : ''}`} onClick={newGameHandler}>
-			New Game
-		</button>
-		{showDifficultySelect && <GameDifficultySelect className={controlsCss.difficultySelect} />}
-		<button className={buttonStyle} onClick={showSolutionHandler}>
-			Show Solution
-		</button>
-		<button className={`${buttonStyle} ${showOptionsSwitches ? buttonCss.pressed : ''}`} onClick={showOptionsHandler}>
-			Options
-		</button>
-		{showOptionsSwitches && <OptionsSwitches className={controlsCss.switches}/>}
-	</Modal>)
-}
+	return (
+		<Modal
+			title="Menu"
+			noBackButton={true}
+			onBack={() => {}}
+			noCloseButton={false}
+			onClose={onCloseHandler}
+		>
+			<button
+				className={`${buttonStyle} ${
+					showDifficultySelect ? buttonCss.pressed : ""
+				}`}
+				onClick={newGameHandler}
+			>
+				New Game
+			</button>
+			{showDifficultySelect && (
+				<GameDifficultySelect className={classes.difficultySelect} />
+			)}
+			<button
+				className={`${buttonStyle} ${
+					showOptionsSwitches ? buttonCss.pressed : ""
+				}`}
+				onClick={showOptionsHandler}
+			>
+				Options
+			</button>
+			{showOptionsSwitches && (
+				<OptionsSwitches className={classes.switches} />
+			)}
+			<button className={buttonStyle} onClick={showSolutionHandler}>
+				Show Solution
+			</button>
+		</Modal>
+	);
+};
 
 export default Menu;
