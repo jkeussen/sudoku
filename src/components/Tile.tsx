97e1cc0ce9c3929c921c44d869d511a9f5107ef0
Boris Boguslavsky <boris@boguslavsky.design>
2023-01-15 20:39:24 -0600

    removed keydown event from tiles

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -66,98 +66,6 @@ const Tile: React.FC<{
 		dispatch(puzzleActions.setActiveSquare(props.id));
 	};
 
-	const onKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
-		e.preventDefault();
-		console.log(e.code)
-		if (!validInputs.includes(e.code)) return;
-
-		let squareToSelect: number | null;
-		let arrowNavigationFlag = false;
-
-		switch (e.code) {
-			case "Escape":
-				// dispatch(puzzleActions.setActiveSquare(null));
-				return;
-			case "KeyM":
-				dispatch(uiActions.setShowMenu(!showMenu))
-				break;
-			case "KeyN":
-				dispatch(uiActions.setnoteModeEnabled(!noteModeEnabled))
-				break;
-			case "KeyP":
-				dispatch(uiActions.setIsTimerPaused(!isTimerPaused))
-				break;
-			case "KeyH":
-				dispatch(
-					puzzleActions.updateUserPuzzle({
-						noteModeEnabled: false,
-						val: solvedString[activeSquare],
-						activeSquare,
-					})
-				);
-				dispatch(puzzleActions.clearNotesOnSquare(activeSquare))
-				dispatch(puzzleActions.incrementHint())
-				break;
-			case "Backspace":
-			case "Digit0":
-			case "KeyD":
-				if (tileIsGiven) return;
-				dispatch(
-					puzzleActions.updateUserPuzzle({
-						noteModeEnabled: noteModeEnabled,
-						val: empty,
-						activeSquare,
-					})
-				);
-				return;
-			case "Digit1":
-			case "Digit2":
-			case "Digit3":
-			case "Digit4":
-			case "Digit5":
-			case "Digit6":
-			case "Digit7":
-			case "Digit8":
-			case "Digit9":
-				if (tileIsGiven) return;
-				let enteredValue = e.code.slice(-1);
-				dispatch(
-					puzzleActions.updateUserPuzzle({
-						noteModeEnabled: noteModeEnabled,
-						val: enteredValue,
-						activeSquare,
-					})
-				);
-				return;
-			case "ArrowUp":
-				if (row === 0) return;
-				squareToSelect = (row - 1) * 9 + col;
-				arrowNavigationFlag = true;
-				break;
-			case "ArrowRight":
-				if (col === 8) return;
-				squareToSelect = row * 9 + col + 1;
-				arrowNavigationFlag = true;
-				break;
-			case "ArrowDown":
-				if (row === 8) return;
-				squareToSelect = (row + 1) * 9 + col;
-				arrowNavigationFlag = true;
-				break;
-			case "ArrowLeft":
-				if (col === 0) return;
-				squareToSelect = row * 9 + col - 1;
-				arrowNavigationFlag = true;
-				break
-			default:
-				break;
-		}
-
-		if (!arrowNavigationFlag) return;
-		// console.log(`Select square [${rowToSelect!}, ${colToSelect!}] (${squareToSelect!})`)
-		dispatch(puzzleActions.setActiveSquare(squareToSelect!));
-	};
-
 	// ! Build CSS classes for the tile
 	const userOrGiven = tileIsGiven ? classes.givenTile : classes.userTile;
 	const isActiveSquare = activeSquare === props.id ? classes.active : "";
@@ -195,7 +103,6 @@ const Tile: React.FC<{
 				value={props.value === empty ? "" : props.value}
 				readOnly
 				onFocus={onFocusHandler}
-				// onKeyDown={onKeyDownHandler}
 			/>
 		</div>
 	);
